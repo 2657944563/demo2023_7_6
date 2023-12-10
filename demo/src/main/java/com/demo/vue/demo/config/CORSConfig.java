1ae6019fcdea34e9ae928bef9b78345e1e106b01
呙启伟 <2657944563@qq.com>
2023-12-11 02:13:23 +0800

    配置日志 添加跨域

---
+++ demo/src/main/java/com/demo/vue/demo/config/CORSConfig.java
@@ -0,0 +1,23 @@
+package com.demo.vue.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 呙启伟
+ * @date 2023/12/10 21:41
+ */
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+
+}
--- demo/src/main/java/com/demo/vue/demo/controller/StudentController.java
+++ demo/src/main/java/com/demo/vue/demo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.demo.vue.demo.controller;
 
+
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.demo.vue.demo.domain.DTO.StudentDTO;
@@ -19,32 +20,34 @@ import java.util.List;
  * @date 2023-12-10 19:55:16
  * 控制层
  */
-@Slf4j
 @RestController
+@Slf4j
 @RequestMapping("/student")
 @AllArgsConstructor
 @Api(tags = "学生信息")
 public class StudentController {
 
     private final IStudentService studentService;
-
     @ApiOperation("获取学生列表")
     @GetMapping("getList")
     public List<Student> list() {
         LambdaQueryWrapper<Student> gt = Wrappers.<Student>lambdaQuery().gt(Student::getAge, 0);
         List<Student> list = studentService.list(gt);
+        log.info("获取学生列表方法");
         return list;
     }
 
     @ApiOperation("添加学生")
     @PostMapping("insertStudent")
     public boolean insertStudent(@RequestBody List<StudentDTO> list) {
+        log.info("添加学生方法");
         return studentService.saveBatch(StudentConverter.INSTANCE.dtosToEntitys(list));
     }
 
     @ApiOperation("获取学生 by id")
     @GetMapping("get/{id}")
     public StudentDTO getStudent(@PathVariable int id) {
+        log.info("通过id获取学生信息方法");
         Student s = studentService.getById(id);
         return StudentConverter.INSTANCE.entityToDto(s);
     }
